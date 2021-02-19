d105051895601131debdfaeafc12f9c53c0a08fd
truscht <thomastrusch@gmx.de>
2021-02-19 20:48:01 +0100

    Create file for Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './img/wayfair-partner-home-logo.png';
 import './App.scss';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <a className="logo" href="/">
-          <img src={logo} className="logo__img" alt="Wayfair Partner Home" />
-        </a>
-        <div className="user-nav">
-          <button className="user-nav__icon user-nav__icon--logged-in" title="User"><span className="user-nav__name chevron">My Username</span></button>
-          <ul className="user-nav__items">
-            <li className="user-nav__item"><a href="#" className="user-nav__link">Account settings</a></li>
-            <li className="user-nav__item"><a href="#" className="user-nav__link">User management</a></li>
-            <li className="user-nav__item"><a href="#" className="user-nav__link">My team</a></li>
-            <li className="user-nav__item"><a href="#" className="user-nav__link">English (UK)</a></li>
-            <li className="user-nav__item"><a href="#" className="user-nav__link">Logout</a></li>
-          </ul>
-        </div>
-      </header>
+      <Header />
       <section className="main">
         <p>
           Lots of paragraphs<br />
