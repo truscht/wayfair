3a44c3e4cb797cd2f3b79744745d961c937316b1
truscht <thomastrusch@gmx.de>
2021-02-19 19:07:26 +0100

    Add user menu

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <a className="logo" href="/">
           <img src={logo} className="App-logo" alt="logo" />
         </a>
-        <button className="user-nav" title="User"></button>
+        <div className="user-nav">
+          <button className="user-nav__icon" title="User"></button>
+          <ul className="user-nav__items">
+            <li className="user-nav__item"><a href="#" className="user-nav__link">Account settings</a></li>
+            <li className="user-nav__item"><a href="#" className="user-nav__link">User management</a></li>
+            <li className="user-nav__item"><a href="#" className="user-nav__link">My team</a></li>
+            <li className="user-nav__item"><a href="#" className="user-nav__link">English (UK)</a></li>
+            <li className="user-nav__item"><a href="#" className="user-nav__link">Logout</a></li>
+          </ul>
+        </div>
       </header>
+      <div>Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br />Test<br /></div>
     </div>
   );
 }
