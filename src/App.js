5ba12c58e8837e496cd72bf2402a55bfd588c91d
truscht <thomastrusch@gmx.de>
2021-02-19 18:35:44 +0100

    Add user icon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import logo from './logo.svg';
+import logo from './wayfair-partner-home-logo.png';
+import iconUser from './icon-user.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://www.wayfair.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wayfair
+        <a className="logo" href="/">
+          <img src={logo} className="App-logo" alt="logo" />
         </a>
+        <button className="user-nav" title="User"></button>
       </header>
     </div>
   );
